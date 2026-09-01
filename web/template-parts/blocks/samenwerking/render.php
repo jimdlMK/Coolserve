70cabe5adde6ea505818c46bf456fbc53f6162d0
Jim <jim@mediakanjers.nl>
2026-09-01 15:41:56 +0200

    samenwerking blok volgens ontwerp

--- web/template-parts/blocks/samenwerking/render.php
+++ web/template-parts/blocks/samenwerking/render.php
@@ -16,51 +16,53 @@
 <section class="mk-samenwerking mk-bg-radius">
     <div class="mk-samenwerking__container">
         <div class="mk-samenwerking__container__inner">
+            <div class="mk-samenwerking__content">
 
-            <?php if ($logo_1 || $logo_2) : ?>
-                <div class="mk-samenwerking__logos">
-                    <?php if ($logo_1) : ?>
-                        <img class="mk-samenwerking__logos__logo" src="<?php echo esc_url($logo_1['url']); ?>" alt="<?php echo esc_attr($logo_1['alt']); ?>">
-                    <?php endif; ?>
-                    <?php if ($logo_1 && $logo_2) : ?>
-                        <span class="mk-samenwerking__logos__x">×</span>
-                    <?php endif; ?>
-                    <?php if ($logo_2) : ?>
-                        <img class="mk-samenwerking__logos__logo" src="<?php echo esc_url($logo_2['url']); ?>" alt="<?php echo esc_attr($logo_2['alt']); ?>">
-                    <?php endif; ?>
-                </div>
-            <?php endif; ?>
+                <?php if ($logo_1 || $logo_2) : ?>
+                    <div class="mk-samenwerking__logos">
+                        <?php if ($logo_1) : ?>
+                            <img class="mk-samenwerking__logos__logo" src="<?php echo esc_url($logo_1['url']); ?>" alt="<?php echo esc_attr($logo_1['alt']); ?>">
+                        <?php endif; ?>
+                        <?php if ($logo_1 && $logo_2) : ?>
+                            <span class="mk-samenwerking__logos__x">×</span>
+                        <?php endif; ?>
+                        <?php if ($logo_2) : ?>
+                            <img class="mk-samenwerking__logos__logo" src="<?php echo esc_url($logo_2['url']); ?>" alt="<?php echo esc_attr($logo_2['alt']); ?>">
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
 
-            <?php if ($label) : ?>
-                <span class="mk-samenwerking__label"><?php echo esc_html($label); ?></span>
-            <?php endif; ?>
+                <?php if ($label) : ?>
+                    <span class="mk-samenwerking__label"><?php echo esc_html($label); ?></span>
+                <?php endif; ?>
 
-            <?php if ($titel) : ?>
-                <h2 class="mk-samenwerking__title"><?php echo esc_html($titel); ?></h2>
-            <?php endif; ?>
+                <?php if ($titel) : ?>
+                    <h2 class="mk-samenwerking__title"><?php echo esc_html($titel); ?></h2>
+                <?php endif; ?>
 
-            <?php if ($tekst) : ?>
-                <div class="mk-samenwerking__tekst"><?php echo wp_kses_post($tekst); ?></div>
-            <?php endif; ?>
+                <?php if ($tekst) : ?>
+                    <div class="mk-samenwerking__tekst"><?php echo wp_kses_post($tekst); ?></div>
+                <?php endif; ?>
 
-            <?php if ($usps) : ?>
-                <div class="mk-samenwerking__usps">
-                    <?php foreach ($usps as $usp) : ?>
-                        <div class="mk-samenwerking__usps__item">
-                            <span class="mk-samenwerking__usps__item__icoon"><?php echo $check_icon; ?></span>
-                            <div class="mk-samenwerking__usps__item__body">
-                                <?php if (!empty($usp['titel'])) : ?>
-                                    <span class="mk-samenwerking__usps__item__body__titel"><?php echo esc_html($usp['titel']); ?></span>
-                                <?php endif; ?>
-                                <?php if (!empty($usp['tekst'])) : ?>
-                                    <span class="mk-samenwerking__usps__item__body__tekst"><?php echo esc_html($usp['tekst']); ?></span>
-                                <?php endif; ?>
+                <?php if ($usps) : ?>
+                    <div class="mk-samenwerking__usps">
+                        <?php foreach ($usps as $usp) : ?>
+                            <div class="mk-samenwerking__usps__item">
+                                <span class="mk-samenwerking__usps__item__icoon"><?php echo $check_icon; ?></span>
+                                <div class="mk-samenwerking__usps__item__body">
+                                    <?php if (!empty($usp['titel'])) : ?>
+                                        <span class="mk-samenwerking__usps__item__body__titel"><?php echo esc_html($usp['titel']); ?></span>
+                                    <?php endif; ?>
+                                    <?php if (!empty($usp['tekst'])) : ?>
+                                        <span class="mk-samenwerking__usps__item__body__tekst"><?php echo esc_html($usp['tekst']); ?></span>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
 
+            </div>
         </div>
     </div>
 </section>
