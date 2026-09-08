746fb03823b63501a70e090bcae01d38f150939f
Jim <jim@mediakanjers.nl>
2026-09-08 08:28:03 +0200

    google reviews doorlink

--- web/template-parts/header/layouts/header-default.php
+++ web/template-parts/header/layouts/header-default.php
@@ -1,7 +1,7 @@
 <header class="mk-header">
 	<div class="mk-header__topbar">
 		<div class="mk-header__topbar__inner">
-			<div class="mk-header__topbar__reviews">
+			<a class="mk-header__topbar__reviews" href="https://www.google.com/maps/search/?api=1&query=Coolserve+Heerenveen" target="_blank" rel="noopener">
 				<svg class="mk-header__topbar__reviews__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" aria-hidden="true">
 					<path fill="#4285F4" d="M23.52 12.273c0-.851-.076-1.67-.218-2.455H12v4.64h6.458a5.52 5.52 0 0 1-2.394 3.622v3.011h3.877c2.269-2.09 3.578-5.166 3.578-8.818Z"/>
 					<path fill="#34A853" d="M12 24c3.24 0 5.955-1.075 7.941-2.909l-3.877-3.01c-1.075.72-2.45 1.145-4.064 1.145-3.126 0-5.771-2.111-6.716-4.948H1.28v3.108C3.256 21.302 7.31 24 12 24Z"/>
@@ -11,8 +11,8 @@
 				<span class="mk-header__topbar__reviews__stars" aria-hidden="true">
 					<span class="mk-header__topbar__reviews__stars__fg"></span>
 				</span>
-				<span class="mk-header__topbar__reviews__score">4,8</span>
-			</div>
+				<strong class="mk-header__topbar__reviews__score">4,9</strong>
+			</a>
 			<?php get_template_part('template-parts/header/nav-top'); ?>
 		</div>
 	</div>
